be3c5e7783bcc61317b5cd17a0be35136c4d440b
Farid Adam <faridibin@gmail.com>
2025-09-05 16:47:57 -0400

    Refactor Eventrel facade to use Illuminate\Support\Facades\Facade and clean up commented code

--- src/Facades/Eventrel.php
+++ src/Facades/Eventrel.php
@@ -1,8 +1,38 @@
 <?php
 
-namespace App;
+namespace Eventrel\Client\Facades;
 
-class Eventrel
+use Illuminate\Support\Facades\Facade;
+
+// /**
+//  * Eventrel facade for Laravel
+//  * 
+//  * @method static \Eventrel\Client\WebhookBuilder event(string $eventType)
+//  * @method static \Eventrel\Client\WebhookResponse sendWebhook(string $eventType, array $payload, ?string $idempotencyKey = null, ?\Carbon\Carbon $scheduledAt = null)
+//  * @method static \Eventrel\Client\WebhookResponse getWebhook(string $webhookId)
+//  * @method static \Eventrel\Client\WebhookListResponse getWebhooks(int $page = 1, array $filters = [])
+//  * @method static \Eventrel\Client\EndpointResponse createEndpoint(string $name, string $url, ?array $events = null, ?int $retryLimit = null, ?array $headers = null)
+//  * @method static array getEndpoints()
+//  * @method static \Eventrel\Client\EndpointResponse getEndpoint(int $endpointId)
+//  * @method static \Eventrel\Client\EndpointResponse updateEndpoint(int $endpointId, array $data)
+//  * @method static bool deleteEndpoint(int $endpointId)
+//  * @method static string regenerateEndpointSecret(int $endpointId)
+//  * @method static \Eventrel\Client\TeamResponse getTeam()
+//  * @method static array getUsage()
+//  * @method static bool inviteMember(string $email, string $role = 'developer')
+//  * @method static array getAllTeams()
+//  * @method static \Eventrel\Client\TeamClient forTeam(string $teamSlug)
+//  * @method static \Eventrel\Client\TeamResponse createTeam(string $name, ?string $slug = null)
+//  *
+//  * @see \Eventrel\Client\EventrelClient
+//  */
+class Eventrel extends Facade
 {
-    // 
+    /**
+     * Get the registered name of the component.
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'eventrel';
+    }
 }
